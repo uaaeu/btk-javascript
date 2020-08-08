0599fb17ad75116add8433a31b1f48b23e6f06ba
uaaeu <yurtmusa@gmail.com>
2020-08-08 12:20:50 +0300

    charAt method

---
+++ methods-on-variables/charAt.js
@@ -0,0 +1,8 @@
+let text = "Google";
+
+let result1 = text.charAt(0);
+let result2 = text.charAt(1);
+let result3 = text.charAt(2);
+let result4 = text.charAt(3);
+let result5 = text.charAt(4);
+let result6 = text.charAt(5);
