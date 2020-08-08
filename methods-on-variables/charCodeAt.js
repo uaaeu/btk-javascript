cfa3c1c4c912582756ccea3214e64e984d02c3d9
uaaeu <yurtmusa@gmail.com>
2020-08-08 12:19:44 +0300

    charCodeAt method

---
+++ methods-on-variables/charCodeAt.js
@@ -0,0 +1,8 @@
+let text = "Google";
+
+let result1 = text.charCodeAt(0);
+let result2 = text.charCodeAt(1);
+let result3 = text.charCodeAt(2);
+let result4 = text.charCodeAt(3);
+let result5 = text.charCodeAt(4);
+let result6 = text.charCodeAt(5);
