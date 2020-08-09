3b096b77fc637afc4523a421286b54f6268fe4cc
uaaeu <yurtmusa@gmail.com>
2020-08-09 23:57:56 +0300

    lastIndexOf

---
+++ methods-on-variables/lastIndexOf.js
@@ -0,0 +1,4 @@
+let value	=	"Javascript Javascript Javascript Javascript Javascript";
+
+let result	=	value.lastIndexOf("Javascript");
+let result	=	value.lastIndexOf("Javascript", 32); // to 32th char.
