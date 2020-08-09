d52c77ac42851940d328e7fd489d1bebb4fc32c0
uaaeu <yurtmusa@gmail.com>
2020-08-09 23:43:05 +0300

    search method

---
+++ methods-on-variables/search.js
@@ -0,0 +1,5 @@
+let value	=	"JavaScript is a scripting or programming language that allows you to implement complex features on web pages.";
+
+let result	=	value.search("script");
+let result	=	value.search(/SCRIPT/i);
+let result	=	value.search(//scRipT/i));
