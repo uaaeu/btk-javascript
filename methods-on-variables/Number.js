50cdbb0138434187615d0aa146644d1bf5cbab84
uaaeu <yurtmusa@gmail.com>
2020-08-11 17:10:44 +0300

    Number method

---
+++ methods-on-variables/Number.js
@@ -0,0 +1,15 @@
+let one = true;
+let two = false;
+let three = "8";
+let four = "8.45";
+let five = new Date();
+let six = "";
+let seven;
+
+let resultA = Number(one);
+let resultB = Number(two);
+let resultC = Number(three);
+let resultD = Number(four);
+let resultE = Number(five);
+let resultF = Number(six);
+let resultG = Number(seven);
