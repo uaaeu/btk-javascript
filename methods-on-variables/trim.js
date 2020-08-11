c0616b28e324934b0c39434a30738191929f77b6
uaaeu <yurtmusa@gmail.com>
2020-08-11 17:39:50 +0300

    trim method

---
+++ methods-on-variables/trim.js
@@ -0,0 +1,8 @@
+let value1 = "     Javascipt     ";
+let value2 = "Training Set";
+let result1 = value1 + " " + value2;-
+alert(result1);
+
+let result2 = value1.trim();
+let warning = result2 + " " + value2;
+alert(warning);
