f1ef45a2da17e03b20b32631a30522ac6935033a
uaaeu <yurtmusa@gmail.com>
2020-08-11 17:30:17 +0300

    substr method

---
+++ methods-on-variables/substr.js
@@ -0,0 +1,6 @@
+let value = "Javascript is the most common script language all over the world.";
+
+let result = value.substr(0, 10);
+let result2 = value.substr(-15);
+let result3 = value.substr(10);
+let result4 = value.substr(-15, 6);
