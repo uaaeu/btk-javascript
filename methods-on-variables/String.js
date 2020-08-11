c3cacb44bc4f8f06afdaac4ee7546bb1cd8cc5f6
uaaeu <yurtmusa@gmail.com>
2020-08-11 17:14:51 +0300

    String method

---
+++ methods-on-variables/String.js
@@ -0,0 +1,15 @@
+let one = Boolean(0);
+let two = Boolean(1);
+let three = 8;
+let four = 8.45;
+let five = new Date();
+let six = ["Uaaeu", "Btk"];
+let seven;
+
+let resultA = String(one);
+let resultB = String(two);
+let resultC = String(three);
+let resultD = String(four);
+let resultE = String(five);
+let resultF = String(six);
+let resultG = String(seven);
