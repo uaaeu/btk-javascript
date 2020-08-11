21e325ea8b0f79c84df992af582b8f6e525e6d39
uaaeu <yurtmusa@gmail.com>
2020-08-11 17:55:19 +0300

    typeof

---
+++ methods-on-variables/typeof.js
@@ -0,0 +1,24 @@
+let name = "Uaaeu"; // String
+let num = 1; // Number
+let bool = true; // Boolean
+let names1 = ["Uaaeu", "C."]; // Array
+let names2 = new Array("Uaaeu", "C."); // Object Array
+let obj = new Object(); // Object
+let undef; // undefined
+let nu = ""; // null
+
+let result1 = name + " : " + typeof name; // String
+
+let result2 = num + " : " + typeof(num); // Number
+
+let result3 = bool + " : " + typeof bool; // Boolean
+
+let result4 = names1 + " : " + typeof(names1); // Array (Object)
+
+let result5 = names2 + " : " + typeof names2; // Object Array (Object)
+
+let result6 = obj + " : " + typeof(obj); // Object
+
+let result7 = undef + " : " + typeof undef; // Undefined
+
+let result8 = nu + " : " + typeof(nu); // Null (Sting)
