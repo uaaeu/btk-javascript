97ec2e3e07d7e3cda273a64756f46819871940d4
uaaeu <yurtmusa@gmail.com>
2020-09-29 10:24:42 +0300

    m (multi line)

--- regular-expressions/regExp.js
+++ regular-expressions/regExp.js
@@ -1,4 +1,4 @@
-let txt = 'hello world i am js. probably the best js. JS THE BEST';
+let txt = 'hello world i am js. probably the best js. JS THE BEST \nJs';
 
 //creating a regex
 let regex = /js/;
@@ -10,17 +10,21 @@ let resulA = txt.match(regexA); //resultA = js
 
 //g (global)
 let regexG = /js/g; //will match every js in text (js js)
-let regexGA = new RegExp('js','g')
+let regexAG = new RegExp('js','g')
 
 //global method
 let control = regex.global //false
 let control = regexG.global //true
 
 //i (ignore case)
-let regexGi = /js/gi; //will match every js, jS, Js or JS in text (js js JS)
-let regexGAi = new RegExp('js','gi')
+let regexGi = /js/gi; //will match every js, jS, Js or JS in text (single line)(js js JS)
+let regexAGi = new RegExp('js','gi')
 
 //ignoreCase method
 let control = regex.ignoreCase //false
 let control = regexG.ignoreCase //false
 let control = regexGi.ignoreCase //true
+
+//m (multi line)
+let regexGiM = /js/gim; //will match every js, jS, Js or JS in text (multi line)(js js JS)
+let regexAGiM = new RegExp('js','gim')
