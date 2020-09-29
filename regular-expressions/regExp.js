978d3979b648fa28adf97d4c45ee0389751e32ef
uaaeu <yurtmusa@gmail.com>
2020-09-29 09:36:14 +0300

    g (global)

--- regular-expressions/regExp.js
+++ regular-expressions/regExp.js
@@ -1,9 +1,13 @@
-let txt = 'hello world i am js.';
+let txt = 'hello world i am js. probably the best js';
 
 //creating a regex
 let regex = /js/;
 let regexA = new RegExp('js');
 
 //.match() method
-let result = txt.match(regex);
-let resulA = txt.match(regexA);
+let result = txt.match(regex); //result = js
+let resulA = txt.match(regexA); //resultA = js
+
+//g (global)
+let regexG = /js/g; //will match every js in text (js js)
+let regexGA = new RegExp('js','g')
